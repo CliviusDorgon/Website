5cac8503d09e8c6f9cc77c3ade12ba3a00fed31f
CliviusDorgon <arnverbelen@yahoo.com>
2024-12-06 12:04:42 +0100

    www.froepie.be

---
+++ unzip.php
@@ -0,0 +1,12 @@
+<?php
+$zip = new ZipArchive();
+$filename = "./Website1.zip"; // Vervang dit door de naam van je zip-bestand
+
+if ($zip->open($filename) === TRUE) {
+    $zip->extractTo('./'); // Geef hier het pad op waar je de bestanden wilt uitpakken
+    $zip->close();
+    echo 'Bestanden succesvol uitgepakt!';
+} else {
+    echo 'Het is niet gelukt om het zip-bestand te openen.';
+}
+?>
